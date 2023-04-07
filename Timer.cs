d80d8111108ec9bd283a91995813d3801be4480e
suhaybEromi <108795119+suhaybEromi@users.noreply.github.com>
2023-04-07 05:04:57 -0700

    C#
    Timer

---
+++ Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ttimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Time.Start();
+        }
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
